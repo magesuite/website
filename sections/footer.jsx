85f99d432c4f92da16093b40a80e9c3e73d21a36
Krzysztof Daniel <krzysztof.daniel@creativestyle.pl>
2019-05-30 23:45:58 +0200

    Migrates footer to styled components

--- sections/footer.jsx
+++ sections/footer.jsx
@@ -1,59 +1,53 @@
 import { colors, distances, mediaQueries, rem } from '../components/styles';
 import MageSuiteLogo from '../components/logos/magesuite-white';
+import styled from 'styled-components';
 
 export default (props) => (
-    <div className="outer">
-        <div className="inner max-width">
-            <div className="col-1">
-                <ul>
-                    <li>
-                        <a href="https://demo.magesuite.io/">Visit demo store</a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/magesuite/">MageSuite on GitHub</a>
-                    </li>
-                </ul>
-            
-            </div>
-            <div className="col-2">
-                <div className="magesuite"><MageSuiteLogo/></div>
-            </div>
-        </div>
-        <style jsx>{`
-            .outer {
-                background: ${colors.d};
-            }
-            .magesuite {
-                opacity: 0.5;
-                width: 10rem;
-                display: inline-block;
-            }
-            .inner {
-                display: flex;
-                padding: ${rem(40)} ${rem(100)};
-            }
-            .col-1 {
-                flex: 1 1;
-            }
-
-            .col-2 {
-                flex: 1 1;
-                text-align: right;
-            }
-
-            a {
-                color: ${colors.l2};
-                font-size: 1rem;
-                text-decoration: none;
-            }
-            ul {
-                display: flex;
-                margin: 0;
-                padding: 0;
-            }
-            li {
-                margin-right: 2rem;
-            }
-        `}</style>
-    </div>
+    <Footer className={props.className}>
+        <Inner>
+            <Area1>
+                <LinksList>
+                    <ListItem><Link href="https://demo.magesuite.io/">Visit demo store</Link></ListItem>
+                    <ListItem><Link href="https://github.com/magesuite/">MageSuite on GitHub</Link></ListItem>
+                </LinksList>
+            </Area1>
+            <Area2>
+                <StyledLogo/>
+            </Area2>
+        </Inner>
+    </Footer>
 )
+
+const Footer = styled.footer`
+    background: ${colors.d};
+`
+const Inner = styled.div`
+    display: flex;
+    padding: ${rem(40)} ${rem(100)};
+`
+
+const Area1 = styled.div`
+    flex: 1 1;
+`
+const Area2 = styled.div`
+    flex: 1 1;
+    text-align: right;
+`
+const LinksList=styled.ul`
+    display: flex;
+    margin: 0;
+    padding: 0;
+`
+const ListItem=styled.li`
+    margin-right: 2rem;
+`
+const Link=styled.a`
+    color: ${colors.l2};
+    font-size: 1rem;
+    text-decoration: none;
+`
+const StyledLogo=styled(MageSuiteLogo)`
+    opacity: 0.5;
+    width: 10rem;
+    display: inline-block;
+`
